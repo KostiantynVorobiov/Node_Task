290e6b847e0db54e1073571a08cfd4055178f82c
Kostian <pyteec.ua@i.ua>
2020-11-16 21:29:39 +0200

    first commit

---
+++ src/main/java/node/node_task/NodeTaskApplication.java
@@ -0,0 +1,13 @@
+package node.node_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeTaskApplication.class, args);
+    }
+
+}
